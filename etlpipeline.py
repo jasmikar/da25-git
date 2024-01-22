c46b845d1fac6a664c21e2e1b1d525fcdb93727e
Jasmin Kara <jasmin.kara@hyperisland.se>
2024-01-22 20:55:16 +0100

    remove comments

--- etlpipeline.py
+++ etlpipeline.py
@@ -1,8 +1,3 @@
-# I have approached my brief from a more rhetoric aspect. Analyzing words, or the lack of them as the other aspect.
-# I have built a pipeline where I've downloaded a file from Kaggle, cleaned those columns that was needed to answer the questions, 
-# and then uploaded the transformed data to Snowflake.
-# Note to the reader: Python offers several powerful libraries for Natural Language Processing (NLP) that could be used for further data cleaning. 
-# Exploring this area too can be done when there's more time.
 
 # ETL-PIPELINE
 
